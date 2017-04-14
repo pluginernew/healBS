76159ecb9b5bd4200c40da78a69f425537559819
pluginernew <nut95@bk.ru>
2017-04-14 09:43:22 +0300

    Update main.php

--- src/HealBS/main.php
+++ src/HealBS/main.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace HealBS;
+
+use pockemine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+class Main extends PluginBase{
+
+pubilc function onEnable(){
+ $this->getServer()->getlogger()->info("[HealBS] Plugin has been enabled");
+ }
+  
+  pubilc function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getname()){
+      case 'heal':
+          $sender->setHealth(20)
+            case 'message':
+        $this->getServer()->broadcast($args[0]);
+        
+       
+    
+    }
+}
