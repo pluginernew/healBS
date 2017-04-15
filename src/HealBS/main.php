e9aa22270cfd3fa9bb16d482c6387cdd91430109
pluginernew <nut95@bk.ru>
2017-04-15 21:53:29 +0300

    Update main.php

--- src/HealBS/main.php
+++ src/HealBS/main.php
@@ -10,7 +10,7 @@ use pocketmine\command\Command;
 class main extends PluginBase{
   
   public function onEnable(){
-    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on")
+    $this->getServer()->getLogger()->info(Color::GREEN."Plugin is on");
   }
   
   public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
